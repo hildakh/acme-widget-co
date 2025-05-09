cec5852c8c7b940cbeb73d8c4afacc9ec1f31a58
Hilda Kh Amiri <hilda.kh.amiri@gmail.com>
2025-05-09 11:59:50 +0400

    Add initial add & total logic

--- basket.rb
+++ basket.rb
@@ -5,4 +5,23 @@ class Basket
     @offers = offers
     @items = []
   end
+
+  def add(item_code)
+    product = @product_catalogue[item_code]
+    @items << product if product
+  end
+
+  def total
+    @items.sum{ |item| item[:price] }
+  end
 end
+
+product_catalogue = {
+  "R01" => { name: "Red Widget", price: 32.95 },
+  "G01" => { name: "Green Widget", price: 24.95 },
+  "B01" => { name: "Blue Widget", price: 7.95 }
+}
+
+basket = Basket.new(product_catalogue, delivery_rules = nil, offers = nil)
+basket.add("R01")
+puts basket.total
