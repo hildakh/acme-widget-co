a973145623f14f4ac5aea84d6f574544fa946798
Hilda Kh Amiri <hilda.kh.amiri@gmail.com>
2025-05-09 14:55:18 +0400

    Add InvalidProductError class for basket validation

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,18 +1,10 @@
+class InvalidProductError < StandardError; end
+
 class Basket
   def initialize(product_catalogue, delivery_rules, offers)
     @product_catalogue = product_catalogue
-    @delivery_rules = delivery_rules || [
-      { min: 90, charge: 0 },
-      { min: 50, charge: 2.95 },
-      { min: 0, charge: 4.95 }
-    ]
-    @offers = offers || [
-      ->(items, product_catalogue) {
-        red_count = items.count { |item| item[:code] == "R01" }
-        red_pairs = red_count / 2
-        red_pairs * (product_catalogue["R01"][:price] / 2)
-      }
-    ]
+    @delivery_rules = delivery_rules
+    @offers = offers
     @items = []
   end
 
@@ -24,18 +16,10 @@ class Basket
   end
 
   def total
-    discount = calculate_discount
+    return 0 unless @items.any?
+
+    discount = @offers.sum { |offer| offer.calculate_discount(@items, @product_catalogue) }
     subtotal = @items.sum{ |item| item[:price] } - discount
-    (subtotal + calculate_delivery_cost(subtotal)).round(2)
-  end
-
-  private
-
-  def calculate_discount
-    @offers.sum { |offer| offer.call(@items, @product_catalogue) }
-  end
-
-  def calculate_delivery_cost(subtotal)
-    @delivery_rules.find { |rule| subtotal >= rule[:min] }[:charge] || 0
+    (subtotal + @delivery_rules.calculate_charges(subtotal)).round(2)
   end
 end
